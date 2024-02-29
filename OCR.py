f793cc9b09e0e84b708a3bef0ebe497efd15a28f
Amanda Caramico <79462423+acaramico@users.noreply.github.com>
2024-02-28 21:37:02 -0300

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -1,12 +1,15 @@
+#https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i
 import pytesseract
 from PIL import Image
 
-def perform_ocr(image_path):
-    image = Image.open(image_path)
-    text = pytesseract.image_to_string(image)
+def perform_ocr(imagem_path):
+    imagem = Image.open(imagem_path)
+    text = pytesseract.image_to_string(imagem, lang='kor')
     return text
 
 
-image_path = "korean_phrases.png"
+imagem_path = "korean_phrases.png"
 print("Gerando OCR...")
-ocr_text = perform_ocr(image_path)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Aluno\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+ocr_text = perform_ocr(imagem_path)
+print(ocr_text)
